818e2cd0386770d97b4c9afa402e4558770a4288
Andrea Olmos <andreaolmos@MBP-de-Andrea.fibertel.com.ar>
2023-09-12 20:38:48 -0300

    como le gusta a marianin

--- letterboxd_project/scrapping_movies_in_lists.py
+++ letterboxd_project/scrapping_movies_in_lists.py
@@ -37,6 +37,7 @@ stars_interpreter = {
 
 #para cada lista del archivo de listas
 for i in range(0, len(datos)):
+    
     try: 
         user_agent_list = [
         "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
@@ -71,129 +72,118 @@ for i in range(0, len(datos)):
                 
                 except requests.exceptions.ChunkedEncodingError:
                     time.sleep(1)
-            else:
-                print("Failed to retrieve url")
-
-
+        
             print("request done")
             print(r.status_code)
-            
-            if r.status_code == 200:
-                tree = lxml.html.fromstring(r.text)
-                table = tree.xpath('//*[@id="content"]/div/div/section/ul')
-                
-                #si la página tiene contenido
-                if(len(table)) != 0:
-                        
-                    #si es la página 1, guardo data de la lista
-                    if number == 1:
-                        #list data
-                        list_title = [title.text for title in tree.xpath('//*[@id="content"]/div/div/section/div[2]/h1')]
-                        list_description = ' '.join([str(element.text) for element in tree.xpath('//*[@id="content"]/div/div/section/div[2]/div/p')])
-                        #get list likes
-                        r = requests.get(f"https://letterboxd.com/ajax/letterboxd-metadata/?likeables=filmlist%3A{list_id}&likeCounts=filmlist%3A{list_id}")
-                        list_likes = json.loads(r.text)["likeables"][0]["count"]
 
-                        #armo el dataframe
-                        df_to_append = pd.DataFrame()
-                        df_to_append = pd.DataFrame(
-                            {'list_id': list_id,
-                            'list_title': list_title,
-                            'list_description': list_description,
-                            'list_likes': list_likes
-                            })
-
-                        #header del csv
-                        header = ["list_id","list_title", "list_description", "list_likes"]
-
-                        # si no existe el csv, se crea con el header
-                        if os.path.isfile(path_lists)==False:
-                            with open(path_lists, 'a') as f:
-                                writer = csv.writer(f)
-                                writer.writerow(header)
-                                df_to_append.to_csv(f, header=False, index=False)
-                                f.flush()
-
-                        # si existe el csv, se appendea el dataframe
-                        else:
-                            with open(path_lists, 'a') as f:
-                                df_to_append.to_csv(f, header=False, index=False)
-                                f.flush()
-
-                        print("list data saved")
-
-                            
-                    if len(table[0].findall("li")) != 0:
-                        
-                    #films data
-                        list_id_arr = [list_id] * len(table[0].findall("li"))
-                            
-                        #movie_id = list(set([element.attrib["data-film-id"] for element in table[0].findall(".//*[@data-film-id]")]))
-
-                        movie_link = [element.attrib["href"] for element in table[0].findall(".//*[@class='headline-2 prettify']/a")]
-            
-                        #movie_stars = [element.text for element in table[0].findall(".//*[@class='film-detail-meta rating-green']/span")]
-                        #print((movie_stars))
-                        details = [element for element in table[0].findall(".//*[@class='film-detail-content']")]
-
-                        movie_stars = []
-                
-                        for calif in details:
-                                
-                                try:
-                                    if calif[1].tag == "p":
-                                        movie_stars.append((calif[1][0].text))
-                                    else:
-                                        movie_stars.append("-")
-
-                                except:
-                                    movie_stars.append("-")
-
-
-                        movie_calification = [stars_interpreter[x] for x in movie_stars]
-
-                        movie_position = [element.text for element in table[0].findall(".//*[@class='list-number']")] 
-
-                        if len(movie_position) == 0:
-                            movie_position = [0] * len(movie_link)
-
-
-                        #armo el dataframe
-                        df_to_append = pd.DataFrame()
-                        df_to_append = pd.DataFrame(
-                            {'list_id': list_id_arr,
-                            #'movie_id': movie_id,
-                            'movie_link': movie_link,
-                            'movie_stars': movie_stars,
-                            'movie_calification': movie_calification,
-                            'movie_position': movie_position
-
-                            })
-
-                        #header del csv
-                        header = ["list_id", "movie_link", "movie_stars", "movie_calification", "movie_position"]
-
-                        # si no existe el csv, se crea con el header
-                        if os.path.isfile(path_movies)==False:
-                            with open(path_movies, 'a') as f:
-                                writer = csv.writer(f)
-                                writer.writerow(header)
-                                df_to_append.to_csv(f, header=False, index=False)
-                                f.flush()
-
-                        # si existe el csv, se appendea el dataframe
-                        else:
-                            with open(path_movies, 'a') as f:
-                                df_to_append.to_csv(f, header=False, index=False)
-                                f.flush()
-
-                        print("movies data saved")
-
-                    else:
-                        break 
-            
-            else:
+            # si la respuesta no es 200: chau
+            if r.status_code != 200:
                 break
 
+            tree = lxml.html.fromstring(r.text)
+            table = tree.xpath('//*[@id="content"]/div/div/section/ul')
+            
+            #si la página no tiene contenido: chau
+            if(len(table)) == 0:
+                break
+            
+            #si es la página 1, guardo data de la lista
+            if number == 1:
+                #list data
+                list_title = [title.text for title in tree.xpath('//*[@id="content"]/div/div/section/div[2]/h1')]
+                list_description = ' '.join([str(element.text) for element in tree.xpath('//*[@id="content"]/div/div/section/div[2]/div/p')])
+                #get list likes
+                r = requests.get(f"https://letterboxd.com/ajax/letterboxd-metadata/?likeables=filmlist%3A{list_id}&likeCounts=filmlist%3A{list_id}")
+                list_likes = json.loads(r.text)["likeables"][0]["count"]
+
+                #armo el dataframe
+                df_to_append = pd.DataFrame()
+                df_to_append = pd.DataFrame(
+                    {'list_id': list_id,
+                    'list_title': list_title,
+                    'list_description': list_description,
+                    'list_likes': list_likes
+                    })
+
+                #header del csv
+                header = ["list_id","list_title", "list_description", "list_likes"]
+
+                # si no existe el csv, se crea con el header
+                if os.path.isfile(path_lists)==False:
+                    with open(path_lists, 'a') as f:
+                        writer = csv.writer(f)
+                        writer.writerow(header)
+                        df_to_append.to_csv(f, header=False, index=False)
+                        f.flush()
+
+                # si existe el csv, se appendea el dataframe
+                else:
+                    with open(path_lists, 'a') as f:
+                        df_to_append.to_csv(f, header=False, index=False)
+                        f.flush()
+
+                print("list data saved")
+
+            # si la tabla no tiene películas, chau 
+            if len(table[0].findall("li")) == 0:
+                break
+                
+            #films data
+            list_id_arr = [list_id] * len(table[0].findall("li"))
+            movie_link = [element.attrib["href"] for element in table[0].findall(".//*[@class='headline-2 prettify']/a")]
+            details = [element for element in table[0].findall(".//*[@class='film-detail-content']")]
+
+            movie_stars = []
+    
+            for calif in details:
+                    
+                    try:
+                        if calif[1].tag == "p":
+                            movie_stars.append((calif[1][0].text))
+                        else:
+                            movie_stars.append("-")
+
+                    except:
+                        movie_stars.append("-")
+
+
+            movie_calification = [stars_interpreter[x] for x in movie_stars]
+            movie_position = [element.text for element in table[0].findall(".//*[@class='list-number']")] 
+
+            if len(movie_position) == 0:
+                movie_position = [0] * len(movie_link)
+
+
+            #armo el dataframe
+            df_to_append = pd.DataFrame()
+            df_to_append = pd.DataFrame(
+                {'list_id': list_id_arr,
+                #'movie_id': movie_id,
+                'movie_link': movie_link,
+                'movie_stars': movie_stars,
+                'movie_calification': movie_calification,
+                'movie_position': movie_position
+                })
+
+            #header del csv
+            header = ["list_id", "movie_link", "movie_stars", "movie_calification", "movie_position"]
+
+            # si no existe el csv, se crea con el header
+            if os.path.isfile(path_movies)==False:
+                with open(path_movies, 'a') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(header)
+                    df_to_append.to_csv(f, header=False, index=False)
+                    f.flush()
+
+            # si existe el csv, se appendea el dataframe
+            else:
+                with open(path_movies, 'a') as f:
+                    df_to_append.to_csv(f, header=False, index=False)
+                    f.flush()
+
+            print("movies data saved")
+    
+    # si todo falla, chau
     except:
         pass        
